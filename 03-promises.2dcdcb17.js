8f5b3cd6f392a7c8ec392016de17bd5b3f1dee44
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2022-07-28 21:26:02 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/goit-js-hw-09@1d725765d228078f078d7057d01b6df598a735a3 🚀

--- 03-promises.2dcdcb17.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');
-//# sourceMappingURL=03-promises.2dcdcb17.js.map
