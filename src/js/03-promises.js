1d725765d228078f078d7057d01b6df598a735a3
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-07-29 00:25:34 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,11 +7,24 @@ const refs = {
 
 // console.log(refs.fieldAmount);
 
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(()=> {
+      if (shouldResolve) {resolve({position, delay})} 
+
+      reject({position, delay})
+    }, delay)
+    
+   
+
+  })
 }
