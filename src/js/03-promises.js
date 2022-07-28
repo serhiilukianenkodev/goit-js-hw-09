afd4e37e017b7abfaa45a3eaf0443c5a8c5464fc
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-07-29 00:09:29 +0300

    task 3 add refs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+const refs = {
+  form: document.querySelector('.form'),
+  fieldDelay: document.querySelector('input[name="delay"]'),
+  fieldStep: document.querySelector('input[name="step"]'),
+  fieldAmount: document.querySelector('input[name="amount"]'),
+}
+
+// console.log(refs.fieldAmount);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
