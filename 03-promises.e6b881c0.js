92612106a8068b3518740087a2b0a29d3a32a770
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2022-07-30 19:57:53 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/goit-js-hw-09@c314d429491b06a2d22be171f1ba4d885af6155b 🚀

--- 03-promises.e6b881c0.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');var e,o;(e=2,o=1500,new Promise(((t,n)=>{const i=Math.random()>.3;setTimeout((()=>{i&&t({position:e,delay:o}),n({position:e,delay:o})}),o)}))).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.e6b881c0.js.map
